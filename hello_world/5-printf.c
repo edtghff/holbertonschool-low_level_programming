5400484ec186f2b76eeb1ccb7e9e2cff7f338322
Ed <halloitsmynew@gmail.com>
2024-10-21 18:13:23 +0400

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  main - the starting point of the program
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
