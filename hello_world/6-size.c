de39d9fa2169357d9b59cd73098e616c8d19d69c
Ed <halloitsmynew@gmail.com>
2024-10-21 18:24:39 +0400

    fix

---
+++ hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * Main - start point
+ * 
+ * Return: always 0
+ */
+
+int main(void)
+{
+    printf("Size of a char: %ld\n", sizeof(char));
+    printf("Size of an int: %ld\n", sizeof(int));
+    printf("Size of a long int: %li\n", sizeof(long));
+    printf("Size of a long long int: %li\n", sizeof(long long int));
+}
