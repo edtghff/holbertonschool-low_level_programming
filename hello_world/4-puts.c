19d3e6eddc7278fcc33b1dab2cc4b2e83ec04f3d
Ed <halloitsmynew@gmail.com>
2024-10-21 18:07:35 +0400

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/**
+ *  main - the starting point of the program
+ *
+ *  Return: Always 0 (Success)
+ */
 int main(void)
-/* Description */	
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
