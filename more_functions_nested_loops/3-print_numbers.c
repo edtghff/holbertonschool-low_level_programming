ff7cf5b3ade8fa1e23043e0098fecbfaa37bad26
Ahmed <halloitsmynew@gmail.com>
2024-10-28 17:53:15 +0400

    Betty fix

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,17 +2,17 @@
 
 /**
  * print_numbers - prints number from 0 to 9
- * 
- * 
+ *
+ *
  */
 
 void print_numbers(void)
 {
-    int i;
+int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        _putchar(i + '0');
-    }
-    _putchar('\n');
+for (i = 0; i < 10; i++)
+{
+_putchar(i + '0');
+}
+_putchar('\n');
 }
