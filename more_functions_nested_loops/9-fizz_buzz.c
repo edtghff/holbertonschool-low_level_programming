1c0c84d42ab43d67b5be12f8287d02570061477d
Ed <halloitsmynew@gmail.com>
2024-10-29 20:01:42 +0400

    fix

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -27,6 +27,12 @@ int main(void)
         {
             printf("%d ", i);
         }
+
+        if (i < 100)
+        {
+            printf(" ");
+        }
     }
+    printf("\n");
     return 0;
 }
