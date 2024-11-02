cf63dff2aa92a080de9676f472aa8b7baae3a633
Ahmed <halloitsmynew@gmail.com>
2024-11-02 20:56:01 +0400

    Fix

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,17 +8,19 @@
 
 void print_rev(char *s)
 {
-    int c = 0;
+int c = 0;
 
-    while (s[c] != '\0')
-    {
-    c++;
-    }
-
-    for (c -= 1; c >= 0; c--)
-    {
-    _putchar(s[c]);
-    }
-
-    _putchar('\n');
+while (s[c] != '\0')
+{
+c++;
 }
+
+for (c -= 1; c >= 0; c--)
+{
+_putchar(s[c]);
+}
+
+_putchar('\n');
+}
+
+
