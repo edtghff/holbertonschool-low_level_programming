4124c215393daa69e242e81f514c43fc040e4824
Ed <halloitsmynew@gmail.com>
2024-11-02 16:32:01 +0400

    strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen_ - returns the length of string
+ *
+ * @s: value
+ * Return: length of @s
+ */
+
+int _strlen(char *s)
+{
+	int i, l = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		l++;
+	}
+	return (l);
+}
+
