64bedcfa6d1ad8889ddd581ccbf4e1aba3771240
Ahmed <halloitsmynew@gmail.com>
2024-11-07 16:28:29 +0400

    Fix

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string
+ * @accept: checks if found in accept
+ * Return: count
+ */
+
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int count = 0;
-	int i, j, accept;
-	
+	int i;
+	int j;
+	int accept;
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		accept = 0;
@@ -24,6 +33,7 @@ unsigned int _strspn(char *s, char *accept)
 			break;
 		}
 	}
-	return count;
+
+	return (count);
 }
 
