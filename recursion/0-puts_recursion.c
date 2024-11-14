f5fd808a19ab5ccfe8dc2e28e4485fff7fb08e9a
Ed <edtaghiyev@gmail.com>
2024-11-15 01:45:43 +0400

    feat

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,1 +1,16 @@
+#include "main.h"
+/**
+* _puts_recursion - prints a string
+* @s: string
+*/
 
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+putchar('\n');
+return;
+}
+putchar(*s);
+_puts_recursion(s + 1);
+}
