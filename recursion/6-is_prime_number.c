a4a23b36adbb998e30b9ff17b8c443a1be988d9b
Ed <edtaghiyev@gmail.com>
2024-11-15 19:29:38 +0400

    Inception. Is it possible?

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -2,15 +2,15 @@
 #include "main.h"
 
 /**
- * is_prime_number - check if input integer is a prime number
+ * is_divisible - helper function to check if a number is divisible
  * @n: input number
+ * @i: potential divisor
  *
- * Return: 1 if prime number, otherwise 0
+ * Return: 1 if not divisible, otherwise 0
  */
 
 int is_divisible(int n, int i)
 {
-
 	if (n <= 1)
 	{
 		return (0);
@@ -19,18 +19,23 @@ int is_divisible(int n, int i)
 	{
 		return (0);
 	}
-	if ((n / i) < i )
+	if ((n / i) < i)
 	{
 		return (1);
 	}
 
 	return (is_divisible(n, i + 1));
-
 }
+
+/**
+ * is_prime_number - check if input integer is a prime number
+ * @n: input number
+ *
+ * Return: 1 if prime number, otherwise 0
+ */
 
 int is_prime_number(int n)
 {
-
-	return (is_divisible(n, 1);
-
+	return (is_divisible(n, 2));
 }
+
--- recursion/6-main.c
+++ recursion/6-main.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include "main.h"
+
 int main(void)
 {
     int r;
