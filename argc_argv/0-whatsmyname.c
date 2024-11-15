d9337ebcf120d70e25c75028032a4b9f022b0340
Ed <edtaghiyev@gmail.com>
2024-11-15 23:23:50 +0400

    It ain't what they call you, it's what you answer to

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -  writes name of function
+ * @argc: argument
+ * @argv: array
+ * Return: 0
+ **/
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
