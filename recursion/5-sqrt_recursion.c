7ad8895e1b79acd61c71a49024c5cd2460b0d06d
Ed <edtaghiyev@gmail.com>
2024-11-15 19:18:58 +0400

    Your subconscious is looking for the dreamer

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -2,35 +2,33 @@
 #include <math.h>
 
 /**
- * sqrt - evaluating from 1 to n
+ * sqrt_recursion - evaluating from 1 to n
  * @a: same as n
  * @b: number that iterates from 1 to n
  *
  * Return: On success 1, otherwise -1
  */
 
-int sqrt(int a, int b)
+int sqrt_recursion(int a, int b)
 {
+if (b * b == a)
+return (b);
+else if (b * b > a)
+return (-1);
 
-	if (b * b == a)
-		return (b);
-	else if (b * b > a)
-		return (-1);
-
-	return (sqrt(a, b + 1));
-
+return (sqrt_recursion(a, b + 1));
 }
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: input number
- * 
- * Return: sqrt function
+ * _sqrt_recursion - wrapper function to start recursion
+ * @n: number to find square root of
+ *
+ * Return: square root of n if exists, otherwise -1
  */
-
 int _sqrt_recursion(int n)
 {
-
-	return (sqrt(n, 1));
-
+if (n < 0)
+return (-1);
+return (sqrt_recursion(n, 1));
 }
+
