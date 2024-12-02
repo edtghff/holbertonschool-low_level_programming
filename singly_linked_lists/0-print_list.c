526291352bc3130f9ea0282879d7a9d97dba268e
Ahmed <halloitsmynew@gmail.com>
2024-12-02 16:40:28 +0400

    added

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,28 +1,21 @@
-#include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
+#include "lists.h"
 /**
  * print_list - prints list
  * @h: pointer
  * Return: count
  */
-
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h) {
-        if (h->str) {
-            printf("[%u] %s\n", h->len, h->str);
-        } else {
-            printf("[0] (nil)\n");
-        }
-        h = h->next;
-        count++;
-    }
-
-    return count;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
-
