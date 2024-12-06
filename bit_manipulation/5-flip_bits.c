6ed241c99edae7cb732e54673f998e6c3651cac5
Ed <edtaghiyev@gmail.com>
2024-12-06 14:18:06 +0400

    Update

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits you would need to
+ * flip to get from one number to another.
+ * @n: number
+ * @m: number
+ * Return: number
+ */
+
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int num = n ^ m;
+	unsigned int pichu = 0;
+	int len = (sizeof(num) * 8);
+
+	while (len >= 0)
+	{
+		if (num & 1)
+			pichu++;
+		num = num >> 1;
+		len--;
+	}
+	return (pichu);
+}
